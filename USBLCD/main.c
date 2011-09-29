ca75bfff77906c73d09fe98a9797c3302666bb24
lolnik <lolnik@d3d9cbc1-074d-d27d-ae7c-3c9a70d908de>
2011-09-29 15:50:19 +0000

    TEMP+USB+LCD WORK

--- USBLCD/main.c
+++ USBLCD/main.c
@@ -9,47 +9,27 @@
 #include <avr/io.h>
 #include <inttypes.h>
 #include <avr/wdt.h>
-#include <avr/interrupt.h>  /* для sei() */
-#include <util/delay.h>     /* для _delay_ms() */
+#include <avr/interrupt.h>  // для sei() 
+#include <util/delay.h>    // для _delay_ms() 
 
- 
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
-#include <avr/pgmspace.h>   /* нужен для usbdrv.h */
+#include "therm_ds18b20.h"
+
+char ch = 0;
+int t;
+int start =0 ;
+int l=0;
+int h=0;
+
+#include <avr/pgmspace.h>   // нужен для usbdrv.h 
 #include "usbdrv/usbdrv.h"
-#include "requests.h"       /* номера custom request, используемые нами */
+#include "requests.h"       // номера custom request, используемые нами 
 #include "avr_compat.h"
 #include "lcd_lib.h" 
-
 //=============================================================================
-//=============================================================================
-
-/*
-typedef struct Bits_t
-{
-	uint8_t Bit0 :1;
-	uint8_t Bit1 :1;
-	uint8_t Bit2 :1;
-	uint8_t Bit3 :1;
-	uint8_t Bit4 :1;
-	uint8_t Bit5 :1;
-	uint8_t Bit6 :1;
-	uint8_t Bit7 :1;
-}Bits;
-#define PortaBits (*((volatile Bits*)&PORTA))
-#define LedPin PortaBits.Bit5
-int main()
-{
-	DDRA = 1 << 5;
-	while(1)
-	{
-		LedPin = 1;//?????? 
-		_delay_ms(100);
-		LedPin = 0;//?????????
-*/
-
 //=============================================================================
 //=============================================================================
 
@@ -68,34 +48,50 @@ int main()
 #define led3 6	//PORTC6
 #define led4 7	//PORTC7
 
-#define PortaBits (*((volatile Bits*)&PORTA))
-#define LedPin PortaBits.Bit5
+//#define PortaBits (*((volatile Bits*)&PORTA))
+//#define LedPin PortaBits.Bit5
+
 
 #define C_DRIVE_VECTOR_Y_UP		1
 #define C_DRIVE_VECTOR_Y_DOWN	2
 //=============================================================================
 //=============================================================================
 
+
+
 //  Function For 
 void DoCommand(long *comm);
 void DisableLeds(void);
+static uchar    currentAddress;
+static uchar    bytesRemaining;
+struct MashineStatus
+{
+	char	C_napravlenie;
+	char	C_povorot;	
+	char	C_OverControl;
+
+};
+
+static struct MashineStatus _mashine;
+
 //=============================================================================
-/* usbFunctionWrite()  usbdrv/usbdrv.h. */
+// usbFunctionWrite()  usbdrv/usbdrv.h. 
 uchar   usbFunctionWrite(uchar *data, uchar len);
 //=============================================================================
-/* usbFunctionRead() usbdrv/usbdrv.h.*/
+// usbFunctionRead() usbdrv/usbdrv.h.
 uchar   usbFunctionRead(uchar *data, uchar len);
 //=============================================================================
-/* Дескриптор выше - только макет, это заглушает драйверы. Репорт, который его 
- *  описывает, состоит из одного байта неопределенных данных. Мы не передаем
- *  наши данные через HID-репорты, вместо этого мы используем custom-запросы.*/
-/* ------------------------------------------------------------------------- */
+// Дескриптор выше - только макет, это заглушает драйверы. Репорт, который его 
+//   описывает, состоит из одного байта неопределенных данных. Мы не передаем
+//   наши данные через HID-репорты, вместо этого мы используем custom-запросы.
+// ------------------------------------------------------------------------- 
 usbMsgLen_t usbFunctionSetup(uchar data[8]);
 
 //=============================================================================
 //=============================================================================
-/* ----------------------------- интерфейс USB ----------------------------- */
-PROGMEM char usbHidReportDescriptor[22] = {    /* дескриптор репорта USB */
+// ----------------------------- интерфейс USB ----------------------------- 
+
+PROGMEM char usbHidReportDescriptor[22] = {    // дескриптор репорта USB 
     0x06, 0x00, 0xff,              // USAGE_PAGE (Generic Desktop)
     0x09, 0x01,                    // USAGE (Vendor Usage 1)
     0xa1, 0x01,                    // COLLECTION (Application)
@@ -108,19 +104,6 @@ PROGMEM char usbHidReportDescriptor[22] = {    /* дескриптор репорта USB */
     0xc0                           // END_COLLECTION
 };
 
-static uchar    currentAddress;
-static uchar    bytesRemaining;
-
-
-struct MashineStatus
-{
-	char	C_napravlenie;
-	char	C_povorot;	
-	char	C_OverControl;
-
-};
-
-static struct MashineStatus _mashine;
 //=============================================================================
 //=============================================================================
 //=============================================================================
@@ -130,27 +113,29 @@ int main(void)
     long  	prev_comm		=	0;
     int 	command_pos 	= 	0;
     int 	del_size 		= 	DEL_CYCLE;
-	int 	lcd_update_time	=	30;	
-	PORTA=0x00;
-	DDRA=0xff;
-	PORTB=0x01;
-	DDRB=0x01;
-	DDRC=0xC5;
-	PORTC=0xC4;
+	int 	lcd_update_time	=	30;
 
-	//DDRD = 0xff;
-	//PORTD=0xff;
-	DDRD = 0x03;
-	PORTD=0x03;
+	PORTA	=	0x00;
+	DDRA	=	0xff;
+	PORTB	=	0x01;
+	DDRB	=	0x01;
+	DDRC	=	0xC5;
+	PORTC	=	0xC4;
+	DDRD 	= 	0x7B;
+	PORTD	=	0x7B;
 
-	GICR|=0x40;
-	MCUCR=0x00;
-	EMCUCR=0x00;
-	GIFR=0x40;
-	TIMSK=0x00;
+	GICR	|=0x40;
+	MCUCR	=0x00;
+	EMCUCR	=0x00;
+	GIFR	=0x40;
+	TIMSK	=0x00;
 
-	ACSR=0x80;
-	SFIOR=0x00;
+	ACSR	=0x80;
+	SFIOR	=0x00;
+
+
+
+
 	//uchar   i;
 
     usbInit();
@@ -159,14 +144,51 @@ int main(void)
     usbDeviceConnect();
     sei();
 
+
 	LCDcursorOFF();
 	LCDclr();
 	LCDGotoXY(3,0);
 	LCDprint("Starting...",11);
 	memset(command,' ',MAX_COMMAND_SIZE);
+   	char buff[BUFF_SIZE];
+	int i=0;
+	char x[20] = {0};
+	for(;;){
+        usbPoll();
 
-    for(;;){
-        //usbPoll();
+		i = (i+1)%10;
+
+		if(i == 5)
+		{
+
+			therm_read_temperature(x);
+			t=tempra();
+			if (start==0)
+			{
+				l=t;
+				h=t;
+			}
+	        start=10;
+			if(t>h){h=t;}
+	        if(t<l){l=t;}
+		    LCDGotoXY(0,0);
+			sprintf(buff,"T%s[%x]h %x l %x",x,t,h,l);
+			LCDprint(buff,sizeof(buff));		
+			//	lcd_string2("T=%s[%x]\nh %x l %x",x,t,h,l);
+	   		t=tempra();
+	   		//int tt;
+			//for (tt=0;tt<5;tt++) //some delay
+			//	_delay_ms(100);
+			if (i%2)
+			{
+				PORTB |= 1;
+			}
+			else
+			{
+				PORTB &= 0xFE;
+			}
+		}
+
         if(command_pos < MAX_COMMAND_SIZE  && (bit_is_clear(PINC,btn1) || bit_is_clear(PINC,btn2) || bit_is_clear(PINC,btn3)))
 		{
 			_delay_ms(del_size*10);
@@ -189,18 +211,17 @@ int main(void)
 			_delay_ms(del_size*30);
         }                 
                
-        if(lcd_update_time == 30)
+        if(lcd_update_time >= 10)
 		{
                 DoCommand(&prev_comm);
 				lcd_update_time = 0;
+
 		}
 		else
 		{
 			lcd_update_time++;
 		}
 
-
-        
         if(bit_is_clear(PINC,btn4))
 		{               
 			LCDclr();
@@ -211,7 +232,6 @@ int main(void)
             _delay_ms(del_size*30);
         }
 
-		//DisableLeds();
     }
     return 0;
 }
@@ -262,10 +282,12 @@ void DoCommand(long *comm)
                 break;  
         case 61:sprintf(buff,"EECR%d:EEARH%d",EECR,EEARH);
                 break;
+        case 589:sprintf(buff,"Error On Temp.");
+                break;
         default:sprintf(buff,"Comm %ld",*comm); 
     }  
     LCDprint(buff,sizeof(buff));
-
+/*
 	if(_mashine.C_napravlenie == C_DRIVE_VECTOR_Y_UP)
 	{
         PORTD |= _BV(1);
@@ -282,19 +304,18 @@ void DoCommand(long *comm)
 		PORTD &= ~_BV(0);
 		PORTD &= ~_BV(1);
 	}
-    
+  */  
 }
 
 //=============================================================================
-/* Дескриптор выше - только макет, это заглушает драйверы. Репорт, который его 
- *  описывает, состоит из одного байта неопределенных данных. Мы не передаем
- *  наши данные через HID-репорты, вместо этого мы используем custom-запросы.*/
-/* ------------------------------------------------------------------------- */
+// Дескриптор выше - только макет, это заглушает драйверы. Репорт, который его 
+//  описывает, состоит из одного байта неопределенных данных. Мы не передаем
+//  наши данные через HID-репорты, вместо этого мы используем custom-запросы.
 usbMsgLen_t usbFunctionSetup(uchar data[8])
 {
 	usbRequest_t    *rq = (void *)data;
-	char mema[32];
-	memcpy(mema,data,32);
+	char mema[12];
+	memcpy(mema,data,12);
 
     if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_VENDOR)
 	{
@@ -314,10 +335,10 @@ usbMsgLen_t usbFunctionSetup(uchar data[8])
         }
 		else if(rq->bRequest == CUSTOM_RQ_GET_STATUS)
 		{
-            static uchar dataBuffer[1];     /* буфер должен оставаться валидным привыходе из usbFunctionSetup */
+            static uchar dataBuffer[1];     // буфер должен оставаться валидным привыходе из usbFunctionSetup 
             dataBuffer[0] = bit_is_set(PORTC,0);//((LED_PORT_OUTPUT & _BV(LED_BIT)) != 0);
-            usbMsgPtr = dataBuffer;         /* говорим драйверу, какие данные вернуть */
-            return 1;                       /* говорим драйверу послать 1 байт */
+            usbMsgPtr = dataBuffer;         // говорим драйверу, какие данные вернуть 
+            return 1;                       // говорим драйверу послать 1 байт 
         }
 		else if(rq->bRequest == 6)
 		{
@@ -328,52 +349,54 @@ usbMsgLen_t usbFunctionSetup(uchar data[8])
 				//LCDstring(rq->wValue.word,32);					
 		}
     }
-	else if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){    /* ?????? HID class */
-        if(rq->bRequest == USBRQ_HID_GET_REPORT){  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
-            /* ????????? ?? ????? ?????? ???? ??? ???????, ?? ????? ???????????? ??????-ID */
+	else if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){    //HID class 
+        if(rq->bRequest == USBRQ_HID_GET_REPORT){  // wValue: ReportType (highbyte), ReportID (lowbyte)       
             bytesRemaining = 128;
             currentAddress = 0;
-            return USB_NO_MSG;  /* ????????????? usbFunctionRead() ??? ????????? ?????? ?????? ?? ?????????? */
+            return USB_NO_MSG;  
         }else if(rq->bRequest == USBRQ_HID_SET_REPORT){
-            /* ????????? ?? ????? ?????? ???? ??? ???????, ?? ????? ???????????? ??????-ID */
             bytesRemaining = 128;
             currentAddress = 0;
-            return USB_NO_MSG;  /* use usbFunctionWrite() ??? ????????? ?????? ??????????? ?? ????? */
+            return USB_NO_MSG;  
         }
     }
-    return 0;   /* default для нереализованных запросов: не возвращаем назад данные хосту */
+    return 0;   // default для нереализованных запросов: не возвращаем назад данные хосту 
 }
 
 //=============================================================================
-/* usbFunctionRead() usbdrv/usbdrv.h.*/
+// usbFunctionRead() usbdrv/usbdrv.h.
+
 uchar   usbFunctionRead(uchar *data, uchar len)
 {
 	PORTC |= _BV(0);
     if(len > bytesRemaining)
         len = bytesRemaining;
     //eeprom_read_block(data, (uchar *)0 + currentAddress, len);
-	memset(data,'A',5);
+//	memset(data,'A',5);
     currentAddress += len;
     bytesRemaining -= len;
     return len;
 }
+
 //=============================================================================
-/* usbFunctionWrite()  usbdrv/usbdrv.h. */
+// usbFunctionWrite()  usbdrv/usbdrv.h. 
+
 uchar   usbFunctionWrite(uchar *data, uchar len)
 {
 	PORTC |= _BV(0);
     if(bytesRemaining == 0)
-        return 1;               /* ????????? ???????? */
+        return 1;              
     if(len > bytesRemaining)
         len = bytesRemaining;
-				LCDclr();
-				LCDGotoXY(0,0);	
-				LCDprint(data,len);
+//				LCDclr();
+//				LCDGotoXY(0,0);	
+//				LCDprint(data,len);
     //eeprom_write_block(data, (uchar *)0 + currentAddress, len);
     currentAddress += len;
     bytesRemaining -= len;
-    return bytesRemaining == 0; /* ??????? 1, ???? ??? ??? ????????? ????? */
+    return bytesRemaining == 0; 
 }
+
 //=============================================================================
 //  Function For disable all leds on PORTC
 void DisableLeds(void)
@@ -389,4 +412,30 @@ void DisableLeds(void)
 }
 //=============================================================================
 //=============================================================================
+//=============================================================================
+
+/*
+typedef struct Bits_t
+{
+	uint8_t Bit0 :1;
+	uint8_t Bit1 :1;
+	uint8_t Bit2 :1;
+	uint8_t Bit3 :1;
+	uint8_t Bit4 :1;
+	uint8_t Bit5 :1;
+	uint8_t Bit6 :1;
+	uint8_t Bit7 :1;
+}Bits;
+#define PortaBits (*((volatile Bits*)&PORTA))
+#define LedPin PortaBits.Bit5
+int main()
+{
+	DDRA = 1 << 5;
+	while(1)
+	{
+		LedPin = 1;//?????? 
+		_delay_ms(100);
+		LedPin = 0;//?????????
+*/
+
 //=============================================================================
